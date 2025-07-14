aea101d08b62aa2a3afe5a581edd20b963135616
ashwithtanniru <ashwithtanniru9@gmail.com>
2025-07-14 08:56:24 +0530

    Add files via upload

---
+++ portoflio web/script.js
@@ -0,0 +1,9 @@
+// Smooth scroll on nav click
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+  link.addEventListener('click', e => {
+    e.preventDefault();
+    document.querySelector(link.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
